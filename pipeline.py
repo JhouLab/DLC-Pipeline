65b74a64d24496972e8036a420cf76528733e7af
Nick S. <nicholasschaffer52@gmail.com>
2024-08-06 11:48:29 -0400

    Added listener
    added new file listener and sleep scheduler

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,25 @@
+import time as Time
+from datetime import datetime, time
 import tkinter
 from tkinter import filedialog
 import os
 import multiprocessing
+from multiprocessing import Manager
 import keyboard
 
 VIDEO_TYPE = ".avi"
+#Boolean flag to set the processing mode. When active, the script will listen for changes in the given directory and analyze only new videos which havent been analyzed yet
+#forces REANALYZE to False
+LISTENER_ACTIVE = True
+#Pauses the analysis during working hours (9am-5pm). For use on machines which are also used as workstations. Will resume analysis after the pause.
+PAUSE_WORK_HOURS = True
 REANALYZE = False
 
+if LISTENER_ACTIVE == True:
+    REANALYZE = False
 
 root = tkinter.Tk()
 root.withdraw()
-
 
 def get_filepath():
     try:
@@ -38,22 +47,69 @@ def get_file():
     return tempdir
 
 
-def subprocess_video(pauseEvent, config, queue):
+def subprocess_video(QUEUE, pauseEvent, config):
     import deeplabcut
     while True:
         if pauseEvent.is_set():
             pauseEvent.wait()
         else:
-            if len(queue) != 0:
-                video = queue.pop(0)
+            if len(QUEUE) != 0:
+                print("==Press 'p' to pause processing, 'r' to restart, and 'q' to quit==")
+                print("Analyzing ", QUEUE[0][0])
+                video = QUEUE.pop(0)
                 deeplabcut.analyze_videos(config= config, videos= video[1], save_as_csv= True, videotype=VIDEO_TYPE)
                 deeplabcut.plot_trajectories(config, video[1])
                 deeplabcut.create_labeled_video(config=config, videos=video[1], videotype= VIDEO_TYPE, draw_skeleton=True)
-                print(len(queue), " More videos to analyze...")
+                Time.sleep(20)
+                print(len(QUEUE), " More videos to analyze...")
             else:
                 print("all videos analyzed, analysis can be closed now with 'q'...")
                 exit(0)
 
+def find_videos(QUEUE, VIDEO_LIST, CSV_LIST, fpath):
+    #look for new videos to add to list
+    new = []
+    for root, dirs, files in os.walk(fpath):
+        for name in files:
+            if name.endswith(VIDEO_TYPE) and (name not in VIDEO_LIST):
+                nName = name.split('.')
+                nName = nName[0]
+                tmp = [nName, os.path.join(root, name)]
+                new.append(tmp)
+                VIDEO_LIST.append(name)
+            if (name.endswith(".csv")) and (name not in CSV_LIST):
+                CSV_LIST.append(name)
+
+    #cross refrence new videos to our complete list of CSV files. If no corresponding CSV file is found, add to queue for analysis
+    for a in range(len(new)):
+        flg = False
+        if REANALYZE == False:
+            for b in range(len(CSV_LIST)):
+                if (new[a][0] in CSV_LIST[b]) and ("DLC" in CSV_LIST[b]):
+                    flg = True
+                    break
+        if flg == False:
+            QUEUE.append(new[a])
+
+
+def subprocess_listener(QUEUE, VIDEO_LIST, CSV_LIST, pauseEvent, fpath):
+    while True:
+        if LISTENER_ACTIVE == True:
+            prev = len(QUEUE)
+            find_videos(QUEUE, VIDEO_LIST, CSV_LIST, fpath)
+            rec = len(QUEUE)
+            if (rec > prev):
+                print("Found ", (rec-prev), " more videos to analyze")
+            Time.sleep(5)
+        if PAUSE_WORK_HOURS == True:
+            now = datetime.now()
+            if time(9, 0) <= now.time() <= time(17, 0):
+                print("Suspending analysis during working hours...")
+                pauseEvent.set()
+            elif pauseEvent.is_set():
+                print("Restarting analysis...")
+                pauseEvent.clear()
+            Time.sleep(60)
 
 
 if __name__ == "__main__":
@@ -63,52 +119,32 @@ if __name__ == "__main__":
     print("Select videos to analyze: ")
     fpath = get_filepath()
 
-    queue = []
-    analyzed = []
+    manager = multiprocessing.Manager()
+    QUEUE = manager.list()
+    VIDEO_LIST = manager.list()
+    CSV_LIST = manager.list()
     print("Finding videos...")
-    for root, dirs, files in os.walk(fpath):
-        for name in files:
-            if name.endswith(VIDEO_TYPE):
-                nName = name.split('.')
-                nName = nName[0]
-                queue.append([nName, os.path.join(root, name)])
-            if name.endswith(".csv"):
-                analyzed.append(name)
+    find_videos(QUEUE, VIDEO_LIST, CSV_LIST, fpath)
+    print("Analyzing ", len(QUEUE), " video files...")
 
-    print("Found ", len(queue), " videos to analyze")
-
-    if REANALYZE == False:
-        if len(analyzed) != 0:
-            tmp = []
-            for a in range(len(queue)):
-                flg = False
-                for b in range(len(analyzed)):
-                    if (queue[a][0] in analyzed[b]) and ("DLC" in analyzed[b]):
-                        print("Eliminated: ", queue[a][0])
-                        flg = True
-                        break
-                if flg == False:
-                    tmp.append(queue[a])
-            queue = tmp
-
-
-    print("Analyzing ", len(queue), " video files...")
-
-    if len(queue) != 0:
+    if len(QUEUE) != 0:
         pauseEvent = multiprocessing.Event()
         print("Starting Analysis...")
-        p = multiprocessing.Process(target=subprocess_video, args=(pauseEvent, config, queue), daemon = True)
+        if LISTENER_ACTIVE == True or PAUSE_WORK_HOURS == True:
+            l = multiprocessing.Process(target=subprocess_listener, args=(QUEUE, VIDEO_LIST, CSV_LIST, pauseEvent, fpath), daemon= True)
+            l.start()
+        p = multiprocessing.Process(target=subprocess_video, args=(QUEUE, pauseEvent, config), daemon= True)
         p.start()
-        print("==Press 'p' to pause processing, 'r' to restart, and 'q' to quit==")
         while p.is_alive():
             if keyboard.is_pressed('p'):
                 pauseEvent.set()
             if keyboard.is_pressed('r'):
                 pauseEvent.clear()
             if keyboard.is_pressed('q'):
+                if LISTENER_ACTIVE == True or PAUSE_WORK_HOURS == True:
+                    l.terminate()
                 p.terminate()
                 break
-
 
     else:
         print("No videos to analyze!")
